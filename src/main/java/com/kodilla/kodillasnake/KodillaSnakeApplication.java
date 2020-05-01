22e480bc7260a925c311d25de89c94ac30326de6
Patrycja05 <patrycja.calka@wp.pl>
2020-05-01 16:04:42 +0200

    First commit

---
+++ src/main/java/com/kodilla/kodillasnake/KodillaSnakeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillasnake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSnakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSnakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillasnake/KodillaSnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillasnake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSnakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
